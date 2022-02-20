956ed116fd9892602528c0b57b81f42e471c1bf2
Justin Miller <jayman1294@gmail.com>
2022-02-19 20:21:06 -0500

    added launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -21,21 +21,46 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchestData() {
-  await axios.post(SPACEX_API_URL, {
+async function loadLaunchData() {
+  const response = await axios.post(SPACEX_API_URL, {
     query: {},
     options: {
+      pagination: false,
       populate: [
         {
           path: 'rocket',
           select: {
             name: 1
+          },
+          path: 'payloads',
+          select: {
+            customers: 1
           }
         }
       ]
     }
   });
+
+  const launchDocs = response.data.docs;
+  for(const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap((payload) => {
+      return payload['customers'];
+    })
+
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers: customers,
+    }
+    console.log(`${launch.flightNumber} ${launch.mission}`)
+  }
 }
+
 
 async function existsLaunchWithId(launchId) {
   return await launches.findOne({ 
@@ -101,7 +126,7 @@ async function abortLaunchById(launchId) {
 } 
 
 module.exports = {
-  loadLaunchestData,
+  loadLaunchData,
   getAllLaunches,
   scheduleNewLaunch,
   abortLaunchById,
